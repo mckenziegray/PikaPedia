e91bf08e13ba957e18de98b9a24444d1d278c1f0
sandys5 <sandys@oregonstate.edu>
2018-08-08 19:20:44 -0700

    putting data in csv test

--- scraperPokeAPI.py
+++ scraperPokeAPI.py
@@ -2,36 +2,54 @@ from lxml import html
 import requests
 import json
 
-urlTemplate = "http://pokeapi.co/api/v2/pokemon/"
+def main():
 
-ListOfPokemonInfo = []
+	f = open("PokeData.csv", "a+")
 
-for Pokemon in range(1,800):
-    url = urlTemplate+str(Pokemon)
-    #print(url)
-    page = requests.get(url)
-    tree = html.fromstring(page.content)
-    #print(page.content)
-    newVar = json.loads(page.content)
+	urlTemplate = "http://pokeapi.co/api/v2/pokemon/"
+
+	ListOfPokemonInfo = []
+
+	for Pokemon in range(1,800):
+		url = urlTemplate+str(Pokemon)
+    		#print(url)
+		page = requests.get(url)
+		tree = html.fromstring(page.content)
+		#print(page.content)
+		newVar = json.loads(page.content.decode('utf-8'))
+		Stats = []    #this will record stats
+		StatsName = []    #this will record stats
+		Name = newVar['name'] #this will return name
+		BaseExperience = newVar['base_experience']     #returns experience gained for defeating this
+		Height = newVar['height']
+
+    		#print(newVar)
+
+		for i in range(6):
+			StatsName.append(newVar['stats'][i]['stat']['name'])
+			Stats.append(newVar['stats'][i]['base_stat'])
+
+		f.write('%s;' % str(Pokemon))
+		f.write('%s;' % str(Name))
+		f.write('%s;' % str(BaseExperience))
+		f.write('%s;' % str(Height))
+
+    		#print("Pokemon #"+str(Pokemon)+": ")
+   		#print(" Name:   "+str(Name))
+    		#print(" BaseExperience: "+str(BaseExperience))
+    		#print(" Height  : "+str(Height))
     
-    Stats = []    #this will record stats
-    StatsName = []    #this will record stats
-    Name = newVar['name'] #this will return name
-    BaseExperience = newVar['base_experience']     #returns experience gained for defeating this
-    Height = newVar['height']
+		for i in range(6):
+			f.write('%s;' % str(StatsName[i]))
+			f.write('%s;' % str(Stats[i]))
+        		#print(" Stat: "+str(StatsName[i])+" -----> "+str(Stats[i]))
 
-    #print(newVar)
-    for i in range(6):
-        #print(newVar['stats'][i]['stat']['name'])
-        #print(newVar['stats'][i]['base_stat'])
-        StatsName.append(newVar['stats'][i]['stat']['name'])
-        Stats.append(newVar['stats'][i]['base_stat'])
+		f.write("\n")
 
-    ###################################################
+	f.close()
 
-    print("Pokemon #"+str(Pokemon)+": ")
-    print(" Name:   "+str(Name))
-    print(" BaseExperience: "+str(BaseExperience))
-    print(" Height  : "+str(Height))
-    for i in range(6):
-        print(" Stat: "+str(StatsName[i])+" -----> "+str(Stats[i]))
+
+
+
+if __name__ == '__main__':
+	main()
