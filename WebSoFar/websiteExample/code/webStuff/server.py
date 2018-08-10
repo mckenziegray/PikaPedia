366d86190b4b5a6d6072a5b94f7b834571413f98
Travis Gray <osugraytr@gmail.com>
2018-08-09 22:01:54 -0700

    Gui Trial #1

--- WebSoFar/websiteExample/code/webStuff/server.py
+++ WebSoFar/websiteExample/code/webStuff/server.py
@@ -1,36 +1,58 @@
 from flask import Flask, render_template, url_for, request
 import os
+import time
+import sys
+import whoosh
+import os
+import csv
+from whoosh.index import create_in
+from whoosh.fields import *
+from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser
 
 app = Flask(__name__)
 
 @app.route("/testpage")
 def birthdays():
+    print("TestPage Triggered")
     dates = {"bulbasaur": 1, "ivysaur": 2, "venusaur": 3, "charmander": 4, "charmeleon": 5, "charizard": 6, "squirtle": 7, "wartortle": 8, "blastoise": 9}
     x = int(100)
     return render_template("testpage.html", dates=dates, x=x)
 
+''' Old homepage
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	print("Someone is at the home page.")
+	print("HomepageTriggered")
 	return render_template('ProjectHomepage.html')
+'''
 
-@app.route('/my-link/')
-def my_link():
-	print('I got clicked!')
-	return 'Click.'
+@app.route('/', methods=['GET', 'POST'])
+def PikaPediaHomepage():
+    if request.method == 'POST':
+        data = request.form
+    else:
+        data = request.args
 
-@app.route('/results/', methods=['GET', 'POST'])
-def results():
-	if request.method == 'POST':
-		data = request.form
-	else:
-		data = request.args
+    print("HomepageTriggered")
+    #Works with this list
+    #
+    #  ToDo: change this into a new list of search results
+    #
+    TempSearchResults = ('bulbasaur', 'ivysaur','venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise')
+    TempSearchResults2 = ('ivysaur','venusaur', 'charmander', 'charmeleon', 'charizard', 'squirtle', 'wartortle', 'blastoise')
+    SearchTerm = "bulbasaur"
 
-	query = data.get('searchterm')
-	print("You searched for: " + query)
-	firstName = ['Ben','Sarah', 'Xandar', 'Ellewyn']
-	lastName = ['McCamish', 'G', 'Quazar', 'Sabbeth']
-	return render_template('results.html', query=query, results=zip(firstName, lastName))
+
+    query = data.get('IndexSearch')
+    TempSearchResults = mainSearch(str(query))
+    lengthList = len(TempSearchResults)
+
+    print("Results: "+ str(TempSearchResults))
+    #time.sleep(5)   # Delays for 5 seconds. You can also use a float value.
+    #TempSearchResults = TempSearchResults2
+
+
+    return render_template('PikaPediaHomepage.html', TempSearchResults=TempSearchResults, lengthList=lengthList)
 
 #Found at URL: http://flask.pocoo.org/snippets/40/
 @app.context_processor
@@ -44,7 +66,56 @@ def dated_url_for(endpoint, **values):
             file_path = os.path.join(app.root_path,
                                  endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
-    return url_for(endpoint, **values)	
+    return url_for(endpoint, **values)
+
+###indexer
+#
+#
+def search(indexer, search_term):
+    results = None
+    SearchResults = ()
+    with indexer.searcher() as searcher:
+        query = MultifieldParser(["title", "content"], schema=indexer.schema).parse(search_term)
+        results = searcher.search(query)
+        print("Number of results: " + str(len(results)))
+        for result in results:
+            SearchResults = SearchResults + (str(result['title']),)
+    return SearchResults
+
+'''Creates the index in the given directory'''
+def index(db_name, index_dir):
+
+    # This is the schema that will be followed by the documents in our index
+    # We may or may not want to add some fields
+    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
+        # Make sure the directory where we will be putting the index exists
+    if not os.path.exists(index_dir):
+        os.mkdir(index_dir)
+        # Create the index inside of the directory
+    indexer = create_in(index_dir, schema)
+        # The writer is used to add documents to the index
+    writer = indexer.writer()
+        # Index each tuple in the csv file
+    with open(db_name, 'r') as csv_file:
+        csv_file = open(db_name, 'r')
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            writer.add_document(title=row['Name'], path='/' + row['Number'], content=' '.join(list(row.values())))
+        writer.commit()
+    return indexer
+
+def mainSearch(SearchTerm):
+    indexer = index("static/Storage/PokeData.csv", "index_dir")
+        # Makeshift temporary search interface
+    #while (True):
+        #search_term = input("Search: ")
+    search_term = SearchTerm
+    #if search_term == "quit":
+    #    break
+    SearchTerm = search(indexer, search_term)
+    print("\n\nResults: "+str(SearchTerm))
+
+    return SearchTerm
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ WebSoFar/websiteExample/code/webStuff/static/Scripts/scraperPokeAPI.py
@@ -0,0 +1,61 @@
+from lxml import html
+import requests
+import json
+import csv
+
+def main():
+	NUM_POKEMON_TO_SCRAPE = 802 # Total 802
+	urlTemplate = "http://pokeapi.co/api/v2/pokemon/"
+
+	with open("PokeData.csv", "w") as f: # 'with' provides free exception handling and closes the file automatically
+		csvwriter = csv.writer(f, delimiter = ',')
+		csvwriter.writerow(('Number','Name','Base Experience','Height','Type 1','Type 2','Ability 1','Ability 2','Ability 3','Speed','Special Defense','Special Attack','Defense','Attack','HP'))
+
+		# print("Before Scrape")
+		for Pokemon in range(1, NUM_POKEMON_TO_SCRAPE):
+			url = urlTemplate+str(Pokemon)
+			page = requests.get(url)
+			json_data = json.loads(page.content.decode('utf-8'))
+			Stats = []    #this will record stats
+			Types = [] 	#this will record Types
+			Abilities = []	#this will record abilities
+			Name = json_data['name'] #this will return name
+			BaseExperience = json_data['base_experience']     #returns experience gained for defeating this
+			Height = json_data['height']
+			#Ability = json_data['abilities'][3]['name']
+
+			test_list = (str(Pokemon),str(Name),str(BaseExperience),str(Height),)
+
+			for num in range(len(json_data['abilities'])):
+				Abilities.append(str(json_data['abilities'][num]['ability']['name']))
+
+			for num in range(len(json_data['types'])):
+				Types.append(str(json_data['types'][num]['type']['name']))
+
+			for i in range(6):
+				Stats.append(json_data['stats'][i]['base_stat'])
+
+			#Types to added to test_list here. I used 2 because this is most possible
+			for num in range(len(Types)):
+				test_list = test_list + (str(Types[num]),)
+			for num in range(2-len(Types)):
+				test_list = test_list + (("None"),)
+		
+			#Ability to added to test_list here. I used 3 because this is most possible
+			for num in range(len(Abilities)):
+				test_list = test_list + (str(Abilities[num]),)
+			for num in range(3-len(Abilities)):
+				test_list = test_list + (("None"),)
+				
+			stat_list = []
+		
+			#Stats to added to test_list here.
+			for i in range(6):
+				stat_list.append(str(Stats[i]))
+				test_list = test_list + (str(stat_list[i]),)
+
+			print(test_list)
+			csvwriter.writerow(test_list)
+
+if __name__ == '__main__':
+	main()
