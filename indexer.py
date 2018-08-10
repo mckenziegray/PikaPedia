ad966c3d115ee513a91838d653f03eade0a662a5
McKenzie <mac.gray.311@gmail.com>
2018-08-09 19:12:13 -0700

    updated indexer and added terminal UI

--- indexer.py
+++ indexer.py
@@ -1,23 +1,25 @@
 import sys
 import whoosh
 import os
+import csv
 from whoosh.index import create_in
 from whoosh.fields import * 
 from whoosh.qparser import QueryParser
 from whoosh.qparser import MultifieldParser
 
 '''Searches through the index using the given search phrase and prints the search results'''
-def search(indexer, searchTerm):
+def search(indexer, search_term):
     results = None
     with indexer.searcher() as searcher:
-        query = MultifieldParser(["title", "content"], schema=indexer.schema).parse(searchTerm)
+        query = MultifieldParser(["title", "content"], schema=indexer.schema).parse(search_term)
         results = searcher.search(query)
         print("Number of results: " + str(len(results)))
         for result in results:
             print(result['title'] + ": " + result['content'][:50] + "...") # Print the first 50 characters of the each result
+        print("")
 
 '''Creates the index in the given directory'''
-def index(documents_dir, index_dir):
+def index(db_name, index_dir):
     # This is the schema that will be followed by the documents in our index
     # We may or may not want to add some fields
     schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
@@ -32,25 +34,25 @@ def index(documents_dir, index_dir):
     # The writer is used to add documents to the index
     writer = indexer.writer()
 
-    # Make sure the documents directory exists
-    if not os.path.exists(documents_dir):
-        raise OSError("Directory " + documents_dir + " does not exist.")
-
-    # Index each csv file in the documents directory
-    for file_name in os.listdir(documents_dir):
-        name, extension = os.path.splitext(file_name)
-        if extension == ".csv":
-            with open(file_name) as csv_file:
-                csv_contents = csv_file.read()
-                writer.add_document(title=name, path=file_name, content=csv_contents)
+    # Index each tuple in the csv file
+    with open(db_name, 'r') as csv_file:
+        csv_file = open(db_name, 'r')
+        csv_reader = csv.DictReader(csv_file)
+        for row in csv_reader:
+            writer.add_document(title=row['Name'], path='/' + row['Number'], content=' '.join(list(row.values())))
 
     writer.commit()
     return indexer
 
 def main():
-    searchTerm = 'bulbasaur'
-    indexer = index(".", "index_dir")
-    search(indexer, searchTerm)
+    indexer = index("PokeData.csv", "index_dir")
+
+    # Makeshift temporary search interface
+    while (True):
+        search_term = input("Search: ")
+        if search_term == "quit":
+            break
+        search(indexer, search_term)
 
 if __name__ == '__main__':
     main()
