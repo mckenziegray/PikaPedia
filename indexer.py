b1c038ec316b48086ffde0c96a43987a689217ab
McKenzie <mac.gray.311@gmail.com>
2018-08-12 12:50:52 -0700

    added/modified fields in indexer schema

--- indexer.py
+++ indexer.py
@@ -12,20 +12,53 @@ from whoosh import qparser
 def search(indexer, search_term):
     results = None
     with indexer.searcher() as searcher:
-        query = MultifieldParser(["title", "content"], schema=indexer.schema, group=qparser.OrGroup).parse(search_term)
+        query = MultifieldParser(
+            [
+                "name", 
+                "id",
+                "type_1",
+                "type_2",
+                "ability_1",
+                "ability_2",
+                "ability_hidden",
+            ],
+            schema=indexer.schema, 
+            group=qparser.OrGroup
+        ).parse(search_term)
         results = searcher.search(query, limit=200) # Return the top 200 results matching the query
         print("Query matches: " + str(len(results)))
         print("Results returned: " + str(results.scored_length()))
+
+        # Results are inaccessible after the searcher closes, so make a copy
         results_list = []
         for result in results:
-            results_list.append({'title': result['title'], 'content': result['content']})# Results are inaccessible after the searcher closes, so make a copy
+            result_dict = {}
+            for key, value in result.items():
+                result_dict[key] = value
+            results_list.append(result_dict)
+
     return results_list
 
 '''Creates the index in the given directory'''
 def index(db_name, index_dir):
     # This is the schema that will be followed by the documents in our index
-    # We may or may not want to add some fields
-    schema = Schema(title=TEXT(stored=True), path=ID(stored=True), content=TEXT(stored=True))
+    schema = Schema(
+        name=TEXT(stored=True), 
+        id=ID(stored=True), 
+        type_1=TEXT(stored=True),
+        type_2=TEXT(stored=True),
+        ability_1=TEXT(stored=True),
+        ability_2=TEXT(stored=True),
+        ability_hidden=TEXT(stored=True),
+        speed=STORED(), # Stored means it won't be indexed and can't be searched for, but is still accessible
+        sp_def=STORED(),
+        sp_atk=STORED(),
+        defense=STORED(),
+        attack=STORED(),
+        hp=STORED(),
+        exp=STORED(),
+        height=STORED()
+    )
 
     # Make sure the directory where we will be putting the index exists
     if not os.path.exists(index_dir):
@@ -42,7 +75,23 @@ def index(db_name, index_dir):
         csv_file = open(db_name, 'r')
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
-            writer.add_document(title=row['Name'], path='/' + row['Number'], content=' '.join(list(row.values())))
+            writer.add_document(
+                name=row['Name'], 
+                id=row['Number'], 
+                type_1=row["Type 1"],
+                type_2=row["Type 2"],
+                ability_1=row["Ability 1"],
+                ability_2=row["Ability 2"],
+                ability_hidden=row["Ability 2"],
+                speed=row["Speed"],
+                sp_def=row["Special Defense"],
+                sp_atk=row["Special Attack"],
+                defense=row["Defense"],
+                attack=row["Attack"],
+                hp=row["HP"],
+                exp=row["Base Experience"],
+                height=row["Height"]
+            )
 
     writer.commit()
     return indexer
